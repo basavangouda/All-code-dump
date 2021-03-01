6e675c82fe3cbad5e5e3f2e9a2de1cfaca4092f4
basavanagouda <basavana003@gmail.com>
2021-03-01 11:48:20 +0530

    Hi

--- BrowserLaunch.py
+++ BrowserLaunch.py
@@ -10,6 +10,7 @@ driver.get("https://www.youtube.com/")
 print(driver.title)
 driver.close()
 
+print(" Hello Enjoy cheyandi")
 driver=webdriver.Edge(executable_path="D:\Software\Drivers\platform-tools_r30.0.4-windows\msedgedriver.exe")
 driver.maximize_window()
 driver.get("https://www.youtube.com/")
